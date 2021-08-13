2c36b3fb0b66538c1e565dbbd26011fe47de4064
Yicheng-1218 <yicheng.881218@gmail.com>
2021-08-13 11:54:31 +0800

    Heroku init

---
+++ app.py
@@ -0,0 +1,61 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/index.html')
+def _():
+    return index()
+
+
+@app.route('/seaturtle.html')
+def seaturtle():
+    return render_template('seaturtle.html')
+
+
+@app.route('/garbagefish.html')
+def garbagefish():
+    return render_template('garbagefish.html')
+
+
+@app.route('/right-1kg.html')
+def package1KG():
+    return render_template('right-1kg.html')
+
+
+@app.route('/raincoat.html')
+def rainCoat():
+    return render_template('raincoat.html')
+
+
+@app.route('/cupshare.html')
+def cupshare():
+    return render_template('cupshare.html')
+
+
+@app.route('/beachcoin.html')
+def beachcoin():
+    return render_template('beachcoin.html')
+
+
+@app.route('/coralreef.html')
+def coralreef():
+    return render_template('coralreef.html')
+
+
+@app.route('/onedaybio.html')
+def onedaybio():
+    return render_template('onedaybio.html')
+
+
+@app.route('/seatrash.html')
+def seatrash():
+    return render_template('seatrash.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
